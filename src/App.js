64a6ccc5afad5ac276513bccb963f46e0d032fd3
Lucas Gaspar de Carvalho <lucas@ecosis.com.br>
2022-07-25 13:54:39 -0300

    att aqvs

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react"
 import { BrowserRouter } from "react-router-dom"
 
 import Routes from "./routes"
+import Header from "./components/Header"
 
 export default function src() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes />
         </BrowserRouter>
     )
