b1045d7c52741515f58216b77a279cd1ce9cd8fe
Lucas Gaspar de Carvalho <lucas@ecosis.com.br>
2022-07-26 15:49:42 -0300

    att aqvs

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,22 +1,25 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import "./style.css"
+import { Container } from "@material-ui/core"
 
 import logo from "../../assets/logo.svg"
 
 export default function Header() {
     return (
-        <header className="container">
-            <Link to="/">
-                <img alt="logo" className="logo" src={logo} />
-            </Link>
+        <Container>
+            <header className="container">
+                <Link to="/">
+                    <img alt="logo" className="logo" src={logo} />
+                </Link>
 
-            <Link to="/reservas" style={{ textDecoration: "none" }}>
-                <div className="reservas">
-                    <strong>Minhas reservas</strong>
-                    <span>0 reservas</span>
-                </div>
-            </Link>
-        </header>
+                <Link to="/reservas" style={{ textDecoration: "none" }}>
+                    <div className="reservas">
+                        <strong>Minhas reservas</strong>
+                        <span>0 reservas</span>
+                    </div>
+                </Link>
+            </header>
+        </Container>
     )
 }
